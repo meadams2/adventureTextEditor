e572be4bf1f7094c917196b91a728a668a14d780
meadams2 <meadams2@bsu.edu>
2024-10-03 10:32:05 -0400

    Day 1
    Main function running, load game initialized, save game init, begin playGame and playNode functions

--- meadams2adventureGameEditor.py
+++ meadams2adventureGameEditor.py
@@ -4,20 +4,38 @@ User should be able to create their own game and save it"""
 import json
 def main():
     userChoice = getMenuChoice()
-    if userChoice == 1:
-        getDefaultGame()
-        print(game)
-    if userChoice == 2:
-        loadGame("adventureGame.json")
-        print("Loaded game.")
-    if userChoice == 3:
-        saveGame()
-    if userChoice == 4:
-        editField()
-        editNode()
-    if userChoice == 5:
-        playGame()
-        playNode()
+    game = getDefaultGame()
+    keepGoing = True
+    while keepGoing:
+        if userChoice == 0:
+            print("Quit")
+            keepGoing = False
+        elif userChoice == 1:
+            print("Load default game")
+#             game = saveGame(game)
+            game = loadGame(game)
+            keepGoing = False
+        elif userChoice == 2:
+#             loadGame("game.json")
+            print("Loaded game.")
+            keepGoing = False
+        elif userChoice == 3:
+            print("Save game.")
+#             saveGame()
+            keepGoing = False
+        elif userChoice == 4:
+            print("Edit or add a node")
+#             editField()
+#             editNode()
+            keepGoing = False
+        elif userChoice == 5:
+            print("Play current game.")
+            playGame(game)
+            playNode(game, "start")
+            keepGoing = False
+        else:
+            print("We've been through this pookie. Quitting game.")
+            keepGoing = False
 
 def getMenuChoice():
     print("""0: Exit
@@ -27,33 +45,9 @@ def getMenuChoice():
 4: Edit or add a node
 5: Play current game""")
     userChoice = input("What will you do?")
+    userChoice = int(userChoice)
     return userChoice
 
-def playGame(currentGame):
-    keepGoing = True
-    while keepGoing:
-        if currentNode == quit
-            keepGoing = False
-        else:
-            currentNode = playNode(game, currentNode)
-def playNode(game, nodeKey):
-    currentNode = game[nodeKey]
-    (description, menu1, node1, menu2, node2) = currentNode
-    print(f"""{description}
-    1: {menu1}
-    2: {menu2}""")
-    nodeChoice = input("1 or 2?")
-    if nodeChoice.isnumeric():
-        nodeChoice = int(nodeChoice)
-        if nodeChoice == 1:
-            newNode = node1
-            currentNode = newNode
-        else:
-            newNode = node2
-            currentNode = newNode
-    else:
-        print("Give me a number, pookie.")
-    return currentNode
 def getDefaultGame():
     game = {
   "start": [
@@ -78,9 +72,51 @@ def getDefaultGame():
     "quit"
   ]
 }
-    print(game)
-    outFile = open("adventureEditor.json", "w")
-    json.dump(game, outFile)
-    outFile.close()
     return game
+def saveGame(currentGame):
+    outFile = open("game.json", "w")
+    json.dump(currentGame, outFile, indent=2)
+    outFile.close()
+    print("Saved game data to game.json.")
+    
+def loadGame(currentGame):
+    file = open("game.json", "r")
+    game = json.load(file)
+    print(game)
+    file.close()
+    
+def playGame(game):
+    keepGoing = True
+    currentNode = "start"
+    while keepGoing:
+        if currentNode == "quit":
+            keepGoing = False
+        else:
+            currentNode = playNode(game, currentNode)
+    return currentNode
+def playNode(game, nodeKey):
+    currentNode = game[nodeKey]
+    if currentNode in game.keys():
+        (description, menu1, node1, menu2, node2) = game[currentNode]
+        print(f"""{description}
+    1: {menu1}
+    2: {menu2}""")
+        nodeChoice = input("1 or 2?")
+        if nodeChoice.isnumeric():
+            nodeChoice = int(nodeChoice)
+            if nodeChoice == 1:
+                nextNode = node1
+                currentNode = newNode
+            elif nodeChoice == 2:
+                nextNode = node2
+            else:
+               print("You bovine. Enter 1 or 2.")
+               nextNode = currentNode
+        else:
+            print("Give me a number, pookie.")
+    else:
+        print("Babes. I don't know what that is. Are you on crack?")
+        nextNode = "quit"
+    return nextNode
 
+main()
